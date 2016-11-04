6bc1b4a5e6fc5c3ab881aadc207de203dd2ad55f
you <you@example.com>
2016-11-04 23:41:17 +0530

    GUI basic home page created. Modifications pending.

--- Home_page.java
+++ Home_page.java
@@ -1,32 +1,87 @@
 package fms_project;
 
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Random;
+import java.awt.event.ActionEvent;
 import javax.swing.*;
 
-
-
 public class Home_page implements ActionListener{
-	private JPanel p1,p2,p3,p4;
-	private JLabel label1;
-	private JButton btnregister,btnlogin;
-	private JFrame frame1;
+	private JPanel p,p1,p0,p2,p3;
+	private JLabel label2;
+	private JButton btnstart,btnexit,btnch1,btnch2,btnch3,btnch4,btnexit2;
+	private JFrame frame,frame2,frame3;
+	private String u1,u2;
+	private int oddeven=1;
+	public static char val='O';
 	Home_page()
 	{
-		frame1=new JFrame("FMS HOME");
-		frame1.setLayout(new GridLayout(4,1));
-		frame1.setLocationRelativeTo(null);
-		frame1.setVisible(true);
-		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame=new JFrame("FMS HOME");
+		frame.setLayout(new GridLayout(3,1));
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		p0=new JPanel(new FlowLayout());
+		p=new JPanel(new FlowLayout());
 		p1=new JPanel(new FlowLayout());
-		p2=new JPanel(new FlowLayout());
-		p3=new JPanel(new FlowLayout());
-		p4=new JPanel(new FlowLayout());
-		btnregister=new JButton("Register");
-			
-		
+		btnstart=new JButton("Register");
+		btnstart.addActionListener(new ActionListener()
+				{
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						register();
+					}
+				});
+		btnexit=new JButton("Login");
+		btnexit.addActionListener(new ActionListener()
+				{
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						login();
+					}					
+				});
+		btnstart.setBackground(Color.ORANGE);
+		btnexit.setBackground(Color.ORANGE);
+		p.add(btnstart);
+		p1.add(btnexit);
+		JLabel label = new JLabel();
+		label.setText("FMS");
+		label.setFont (label.getFont ().deriveFont (30.0f));
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		p0.add(label);
+		frame.add(p0);
+		frame.add(p);
+		frame.add(p1);
+		frame.setSize(400, 300);
 	}
 	
+     
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Home_page();
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String arg=e.getActionCommand();
+		if(arg=="Start Game")
+		{
+			
+			this.register();
+		}
+		else if(arg=="Exit");
+		{
+			this.login();
+		}
+		
+	}
 	public void register()
 	{
 		
@@ -35,24 +90,6 @@ public class Home_page implements ActionListener{
 	{
 		
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new Home_page();
-		
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		String arg=e.getActionCommand();
-		if(arg=="Register")
-		{
-			
-			this.register();
-		}
-		else if(arg=="Login");
-		{
-			this.login();
-		}
-		
-}
+	
+	
 }
