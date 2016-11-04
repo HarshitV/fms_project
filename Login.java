e5d2f92ece2e2e2cdb0ccfeb148228b7de1d6c54
you <you@example.com>
2016-11-04 23:46:30 +0530

    Login page.

---
+++ Login.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Login{
+	private String username;
+	private String password;
+	//private Logout logo;
+
+	public void get_info(){
+
+	}
+
+	public void check_info(){
+
+	}
+
+	//public void open_userscreen( User a){
+
+	//}
+}
