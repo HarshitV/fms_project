c0bcb2e4536d5cdd24bc32aee350fe11f24234f8
HarshitV <harshitv@rocketmail.com>
2016-11-05 19:58:38 +0530

    Add files via upload
    New java file created to carry out logout processes

---
+++ Logout.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Logout{
+	public void display_homepage(){
+
+	}
+
+	public void display_logout_message(String a){
+
+	}
+}
