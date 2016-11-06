4c32220697dbe22b0c9eaa0c2f439530ab0082a2
anirudh15015 <anirudh15015@iiitd.ac.in>
2016-11-06 22:59:40 +0530

    Add files via upload
    it contains all the details and functions of staff class as per our program

---
+++ Staff.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Staff{
+	String department;
+	
+	public void send_log_req(){
+
+	}
+
+	public void send_leave(){
+
+	}
+
+	public void update_status(){
+
+	}
+
+	public void generate_report(){
+		
+	}
+}
