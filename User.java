965279f8273d9495845b218fd44e0894bf7e2ccb
anirudh15015 <anirudh15015@iiitd.ac.in>
2016-11-07 23:48:54 +0530

    Add files via upload
    User class that is going to be extended by other classes such as Supervisor and so on

---
+++ User.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class User{
+	protected String name;
+	protected String passwd;
+	protected String userid;
+	protected String type;
+	protected String dob;
+	protected String address;
+}
