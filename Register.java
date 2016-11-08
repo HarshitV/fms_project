6103829bea8f132f7a16e89597c5b68567f61ab4
HarshitV <harshitv@rocketmail.com>
2016-11-09 02:40:47 +0530

    Add files via upload
    new class Register created with its attributes and methods

---
+++ Register.java
@@ -0,0 +1,32 @@
+package fms_project;
+
+public class Register {
+	private int check_result;
+	private String[] info=new String[7];
+	
+	public String generate_userID()
+	{
+		return null;		
+	}
+	
+	public void get_info()
+	{
+		
+	}
+	
+	public void check_info()
+	{
+		
+	}
+	
+	public void sendfor_approval(String recipient)
+	{
+		
+	}
+	
+	public void display_homepage()
+	{
+		
+	}
+	
+}
