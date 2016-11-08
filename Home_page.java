949690ed4ab5bf3c96d83ad0970bb5fb3d3377d8
HarshitV <harshitv@rocketmail.com>
2016-11-09 02:47:37 +0530

    Add files via upload
    Major internal code changes- new method added to Register class [createGUI]

--- Home_page.java
+++ Home_page.java
@@ -8,9 +8,10 @@ public class Home_page implements ActionListener{
 	private JPanel panel3,panel2,panel1,panel4,bpane,fpane,bpane2,fpane2;
 	private JButton btnregister,btnlogin,ok,ok2,cancel,cancel2;
 	private JFrame frame1,frame2,frame3;
+	Login lgn=new Login();
+	Register rgr=new Register();
 	Home_page()
 	{
-		Login lgn=new Login();
 		frame1=new JFrame("FMS HOME");
 		frame1.setLayout(new GridLayout(5,1));
 		
@@ -27,7 +28,7 @@ public class Home_page implements ActionListener{
 					@Override
 					public void actionPerformed(ActionEvent e) {
 						// TODO Auto-generated method stub
-						register();
+						rgr.createGUI();
 					}
 				});
 		btnlogin=new JButton("Login");
@@ -83,99 +84,17 @@ public class Home_page implements ActionListener{
 		String arg=e.getActionCommand();
 		if(arg=="Register")
 		{
-			
-			this.register();
+			//Register rgr=new Register();
+			rgr.createGUI();
+			//this.register();
 		}
 		else if(arg=="Login");
 		{
-			Login lgn=new Login();
+			//Login lgn=new Login();
 			lgn.createGUI();
 			//this.login();
 		}	
 	}
-	public void register()
-	{
-		//frame1.setVisible(false);
-		frame2=new JFrame("FMS REGISTER");
-		//frame2.setLayout(new GridLayout(6,1));
-		frame2.setVisible(true);
-		frame2.setSize(450,200);
-		frame2.setLocationRelativeTo(null);
-		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		bpane=new JPanel();
-		fpane=new JPanel();
-		JComboBox<String> deptypes = new JComboBox<String>();
-		deptypes.addItem("Select option");
-		deptypes.addItem("Staffer");
-		deptypes.addItem("Supervisor");
-		JLabel name=new JLabel("Enter your name: ");
-		name.setFont (name.getFont ().deriveFont (15.0f));
-		JLabel uname=new JLabel("Enter username: ");
-		uname.setFont (uname.getFont ().deriveFont (15.0f));
-		JLabel type=new JLabel("Enter department:");
-		type.setFont (type.getFont ().deriveFont (15.0f));
-		JLabel pwd=new JLabel("Enter password: ");
-		pwd.setFont (pwd.getFont ().deriveFont (15.0f));
-		JLabel DOB=new JLabel("Enter date of birth");
-		DOB.setFont (DOB.getFont ().deriveFont (15.0f));
-		JLabel address=new JLabel("Enter your address: ");
-		address.setFont (address.getFont ().deriveFont (15.0f));
-		ok = new JButton("OK");
-		ok.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				frame2.setVisible(false);
-			
-			}
-			
-		});
-		
-        cancel = new JButton("Cancel");
-        cancel.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				frame2.dispose();
-			
-			}
-			
-		});
-        fpane.setLayout(new GridLayout(6,1));
-        bpane.setLayout(new FlowLayout());
-        JTextField usname,names,dobs,addr;
-        JPasswordField pswd;
-        usname=new JTextField("");
-       // Font bigFont = usname.getFont().deriveFont(Font.PLAIN, 500f);
-        //usname.setFont(bigFont);
-        usname.setPreferredSize( new Dimension( 200, 24 ) );
-        pswd=new JPasswordField();
-        //pswd=new JTextField("");
-        names=new JTextField("");
-        //types=new JTextField("");
-        dobs=new JTextField("");
-        addr=new JTextField("");
-        fpane.add(uname);
-        fpane.add(usname);
-        fpane.add(pwd);
-        fpane.add(pswd);
-        fpane.add(name);
-        fpane.add(names);
-        fpane.add(type);
-        fpane.add(deptypes);
-        //fpane.add(types);
-        fpane.add(DOB);
-        fpane.add(dobs);
-        fpane.add(address);
-        fpane.add(addr);
-        bpane.add(ok);
-        bpane.add(cancel);
-        frame2.getContentPane().add(fpane);
-        frame2.add(bpane, BorderLayout.PAGE_END);
-	}
-	
 	
 	
 }
--- Register.java
+++ Register.java
@@ -1,8 +1,109 @@
 package fms_project;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
 public class Register {
+	private JFrame frame2;
+	private JPanel fpane,bpane;
+	private JButton ok,cancel;
 	private int check_result;
 	private String[] info=new String[7];
+	
+	public void createGUI()
+	{
+		//frame1.setVisible(false);
+				frame2=new JFrame("FMS REGISTER");
+				//frame2.setLayout(new GridLayout(6,1));
+				frame2.setVisible(true);
+				frame2.setSize(450,200);
+				frame2.setLocationRelativeTo(null);
+				frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				bpane=new JPanel();
+				fpane=new JPanel();
+				JComboBox<String> deptypes = new JComboBox<String>();
+				deptypes.addItem("Select option");
+				deptypes.addItem("Staffer");
+				deptypes.addItem("Supervisor");
+				JLabel name=new JLabel("Enter your name: ");
+				name.setFont (name.getFont ().deriveFont (15.0f));
+				JLabel uname=new JLabel("Enter username: ");
+				uname.setFont (uname.getFont ().deriveFont (15.0f));
+				JLabel type=new JLabel("Enter department:");
+				type.setFont (type.getFont ().deriveFont (15.0f));
+				JLabel pwd=new JLabel("Enter password: ");
+				pwd.setFont (pwd.getFont ().deriveFont (15.0f));
+				JLabel DOB=new JLabel("Enter date of birth");
+				DOB.setFont (DOB.getFont ().deriveFont (15.0f));
+				JLabel address=new JLabel("Enter your address: ");
+				address.setFont (address.getFont ().deriveFont (15.0f));
+				ok = new JButton("OK");
+				ok.addActionListener(new ActionListener(){
+
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						// TODO Auto-generated method stub
+						frame2.setVisible(false);
+					
+					}
+					
+				});
+				
+		        cancel = new JButton("Cancel");
+		        cancel.addActionListener(new ActionListener(){
+
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						// TODO Auto-generated method stub
+						frame2.dispose();
+					
+					}
+					
+				});
+		        fpane.setLayout(new GridLayout(6,1));
+		        bpane.setLayout(new FlowLayout());
+		        JTextField usname,names,dobs,addr;
+		        JPasswordField pswd;
+		        usname=new JTextField("");
+		       // Font bigFont = usname.getFont().deriveFont(Font.PLAIN, 500f);
+		        //usname.setFont(bigFont);
+		        usname.setPreferredSize( new Dimension( 200, 24 ) );
+		        pswd=new JPasswordField();
+		        //pswd=new JTextField("");
+		        names=new JTextField("");
+		        //types=new JTextField("");
+		        dobs=new JTextField("");
+		        addr=new JTextField("");
+		        fpane.add(uname);
+		        fpane.add(usname);
+		        fpane.add(pwd);
+		        fpane.add(pswd);
+		        fpane.add(name);
+		        fpane.add(names);
+		        fpane.add(type);
+		        fpane.add(deptypes);
+		        //fpane.add(types);
+		        fpane.add(DOB);
+		        fpane.add(dobs);
+		        fpane.add(address);
+		        fpane.add(addr);
+		        bpane.add(ok);
+		        bpane.add(cancel);
+		        frame2.getContentPane().add(fpane);
+		        frame2.add(bpane, BorderLayout.PAGE_END);
+	}
 	
 	public String generate_userID()
 	{
