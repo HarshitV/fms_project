4b6f2f2075f1eb9ac72e3269a4db6e25ab293e38
anirudh15015 <anirudh15015@iiitd.ac.in>
2016-11-08 22:47:38 +0530

    To be used when an user wants a leave from his senior
    To be used when an user wants a leave from his senior

---
+++ Leave.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Leave{
+	String reason;
+	String date;
+	String start_date;
+	int range;
+	String end_date;
+	String user;
+	String recipient;
+
+	public void get_info(){
+
+	}
+
+	public void add_info(){
+		
+	}
+}
