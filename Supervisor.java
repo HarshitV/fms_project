38d23eedc323f6cf871ca82b1f8a9f8c0579bde4
anirudh15015 <anirudh15015@iiitd.ac.in>
2016-11-08 22:52:47 +0530

    inherits properties from user and contains info about supervisor

---
+++ Supervisor.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Supervisor extends User{
+	String department;
+
+	public void approve_user(){
+
+	}
+
+	public void reject_user(){
+
+	}
+
+	public void view_user(){
+
+	}
+
+	public void delete_user(){
+
+	}
+
+	public void assign_task(){
+
+	}
+
+	public void accept(){
+
+	}
+
+	public void reject(){
+
+	}
+}
