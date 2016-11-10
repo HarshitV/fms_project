b3d49189ffd34d6e93f492bca56464ba8fb2caac
HarshitV <harshitv@rocketmail.com>
2016-11-10 19:16:24 +0530

    Commit 20
    new class GM created

---
+++ GM.java
@@ -0,0 +1,47 @@
+package fms_project;
+
+import java.util.ArrayList;
+
+public class GM {
+
+	//ArrayList<logistic_req> req_list=new ArrayList<logistic_req>();
+	//ArrayList<leave> leave_list=new ArrayList<leave>();
+	
+	public void approve_user()
+	{
+		
+	}
+	
+	public void reject_user()
+	{
+		
+	}
+	
+	public void view_user()
+	{
+		
+	}
+	
+	public void delete_user()
+	{
+		
+	}
+	
+	public void assign_task()
+	{
+		
+	}
+	
+	public void accept()
+	{
+		
+	}
+	
+	public void reject()
+	{
+		
+	}
+	
+	
+	
+}
