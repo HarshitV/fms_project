a9d8ffe477d9fb04079bab73022f99b43255e89c
HarshitV <harshitv@rocketmail.com>
2016-11-12 00:46:27 +0530

    Add files via upload
    Gridbox layout used for GM class and multiple buttons along with a jtable added

--- GM.java
+++ GM.java
@@ -1,9 +1,15 @@
 package fms_project;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -15,7 +21,7 @@ public class GM extends User{
 
 	private static JFrame frame4;
 	private static JPanel fpane2,bpane2;
-	private static JButton btn1;
+	private static JButton btn1,btn2,btn3,btn4,btn5;
 	private static JLabel date;
 	
 	//ArrayList<logistic_req> req_list=new ArrayList<logistic_req>();
@@ -23,34 +29,337 @@ public class GM extends User{
 	
 	public static void createGUI()
 	{
-		
+		fpane2=new JPanel();
+		bpane2=new JPanel();
 		DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss, dd MMM yyyy ");
 		Calendar cal = Calendar.getInstance();
 		date=new JLabel("Logged in at: " + dateFormat.format(cal.getTime()));		
 		date.setFont (date.getFont ().deriveFont (15.0f));
-		date.setHorizontalAlignment(JLabel.RIGHT);
+		JLabel loggeduser=new JLabel("Logged in as ADMIN");
+		loggeduser.setFont (loggeduser.getFont ().deriveFont (15.0f));
+		String[] columns = new String[] { "Home", "Staff", "Logistics", "Reports","Requests" };
+		Object[][] data = new Object[][] { {1, "John", 40.0, false,"yyo" },};
+		JTable table = new JTable(data, columns);
+		//table.setSize(200, 300);
+		//table.setBounds(30,40,200,300);
+		//table.setRowHeight(1, 5);
+		table.setEnabled(false);
+		JScrollPane sp=new JScrollPane(table);  
+		
 		ImageIcon imageIcon = new ImageIcon("C:/Users/Harshit/workspace/fms_project/src/fms_project/logonew.png"); // load the image to a imageIcon
 		Image image = imageIcon.getImage(); // transform it 
-		Image newimg = image.getScaledInstance(250, 120,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+		Image newimg = image.getScaledInstance(150, 80,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
 		imageIcon = new ImageIcon(newimg);
+		JLabel logo = new JLabel(imageIcon);
+		btn1=new JButton("Home");
+		btn2=new JButton("Staff");
+		btn3=new JButton("Logistics");
+		btn4=new JButton("Reports");
+		btn5=new JButton("Requests");
+		btn1.setPreferredSize(new Dimension(200, 60));
+		btn2.setPreferredSize(new Dimension(200, 60));
+		btn3.setPreferredSize(new Dimension(200, 60));
+		btn4.setPreferredSize(new Dimension(200, 60));
+		btn5.setPreferredSize(new Dimension(200, 60));
+		btn1.addActionListener(new ActionListener()
+				{
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				fpane2.removeAll();
+				fpane2.add(sp,new GridBagConstraints(
+						0, // gridx
+		                0, // gridy
+		                1, // gridwidth
+		                1, // gridheight
+		                1, // weightx
+		                1, // weighty
+		                GridBagConstraints.CENTER, // anchor <------------
+		                GridBagConstraints.NONE, // fill
+		                new Insets(125, // inset top
+		                200, // inset left
+		                100, // inset bottom
+		                0), // inset right
+		                0, // ipadx
+		                0)
+						);
+				fpane2.add(btn1,new GridBagConstraints(
+						0, // gridx
+		                0, // gridy
+		                1, // gridwidth
+		                1, // gridheight
+		                1, // weightx
+		                1, // weighty
+		                GridBagConstraints.NORTHWEST, // anchor <------------
+		                GridBagConstraints.NONE, // fill
+		                new Insets(125, // inset top
+		                20, // inset left
+		                0, // inset bottom
+		                0), // inset right
+		                0, // ipadx
+		                0)
+						);
+				fpane2.add(btn2,new GridBagConstraints(
+						0, // gridx
+		                0, // gridy
+		                1, // gridwidth
+		                1, // gridheight
+		                1, // weightx
+		                1, // weighty
+		                GridBagConstraints.NORTHWEST, // anchor <------------
+		                GridBagConstraints.NONE, // fill
+		                new Insets(190, // inset top
+		                20, // inset left
+		                0, // inset bottom
+		                0), // inset right
+		                0, // ipadx
+		                0)
+						);
+				fpane2.add(btn3,new GridBagConstraints(
+						0, // gridx
+		                0, // gridy
+		                1, // gridwidth
+		                1, // gridheight
+		                1, // weightx
+		                1, // weighty
+		                GridBagConstraints.NORTHWEST, // anchor <------------
+		                GridBagConstraints.NONE, // fill
+		                new Insets(255, // inset top
+		                20, // inset left
+		                0, // inset bottom
+		                0), // inset right
+		                0, // ipadx
+		                0)
+						);
+				fpane2.add(btn4,new GridBagConstraints(
+						0, // gridx
+		                0, // gridy
+		                1, // gridwidth
+		                1, // gridheight
+		                1, // weightx
+		                1, // weighty
+		                GridBagConstraints.NORTHWEST, // anchor <------------
+		                GridBagConstraints.NONE, // fill
+		                new Insets(320, // inset top
+		                20, // inset left
+		                0, // inset bottom
+		                0), // inset right
+		                0, // ipadx
+		                0)
+						);	
+				fpane2.add(btn5,new GridBagConstraints(
+						0, // gridx
+		                0, // gridy
+		                1, // gridwidth
+		                1, // gridheight
+		                1, // weightx
+		                1, // weighty
+		                GridBagConstraints.NORTHWEST, // anchor <------------
+		                GridBagConstraints.NONE, // fill
+		                new Insets(385, // inset top
+		                20, // inset left
+		                0, // inset bottom
+		                0), // inset right
+		                0, // ipadx
+		                0)
+						);
+				fpane2.add(loggeduser,new GridBagConstraints(0, // gridx
+		                0, // gridy
+		                1, // gridwidth
+		                1, // gridheight
+		                1, // weightx
+		                1, // weighty
+		                GridBagConstraints.NORTHWEST, // anchor <------------
+		                GridBagConstraints.NONE, // fill
+		                new Insets(0, // inset top
+		                0, // inset left
+		                0, // inset bottom
+		                0), // inset right
+		                0, // ipadx
+		                0));
+				fpane2.add(date,new GridBagConstraints(0, // gridx
+		                0, // gridy
+		                1, // gridwidth
+		                1, // gridheight
+		                1, // weightx
+		                1, // weighty
+		                GridBagConstraints.NORTHEAST, // anchor <------------
+		                GridBagConstraints.NONE, // fill
+		                new Insets(0, // inset top
+		                0, // inset left
+		                0, // inset bottom
+		                0), // inset right
+		                0, // ipadx
+		                0));
+				fpane2.add(logo,new GridBagConstraints(0, // gridx
+		                0, // gridy
+		                1, // gridwidth
+		                1, // gridheight
+		                1, // weightx
+		                1, // weighty
+		                GridBagConstraints.NORTH, // anchor <------------
+		                GridBagConstraints.NONE, // fill
+		                new Insets(10, // inset top
+		                0, // inset left
+		                0, // inset bottom
+		                0), // inset right
+		                0, // ipadx
+		                0));
+				fpane2.revalidate();
+				fpane2.repaint();
+				
+				/*frame4.getContentPane().removeAll();
+				frame4.add(btn1);
+				frame4.revalidate();
+				frame4.repaint();*/
+				}
+	});
+		
+		
+		
+		GridBagLayout gridbag = new GridBagLayout();
 		
 		frame4=new JFrame("Admin Home");
 		frame4.setVisible(true);
 		frame4.setSize(350,120);
 		frame4.setLocationRelativeTo(null);
 		frame4.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		fpane2=new JPanel();
-		bpane2=new JPanel();
-		//fpane2.setLayout(new BoxLayout);
-		//fpane2.add(date);
-		JLabel logo = new JLabel(imageIcon);
-		logo.setVerticalAlignment(JLabel.TOP);
-		frame4.add(logo,BorderLayout.CENTER);
-		frame4.add(date,BorderLayout.NORTH);
-		Toolkit tk = Toolkit.getDefaultToolkit();
-		int xSize = ((int) tk.getScreenSize().getWidth());
-		int ySize = ((int) tk.getScreenSize().getHeight());
-		frame4.setSize(xSize,ySize);
+		
+		fpane2.setLayout(gridbag);
+		
+		fpane2.add(loggeduser,new GridBagConstraints(0, // gridx
+                0, // gridy
+                1, // gridwidth
+                1, // gridheight
+                1, // weightx
+                1, // weighty
+                GridBagConstraints.NORTHWEST, // anchor <------------
+                GridBagConstraints.NONE, // fill
+                new Insets(0, // inset top
+                0, // inset left
+                0, // inset bottom
+                0), // inset right
+                0, // ipadx
+                0));
+		fpane2.add(date,new GridBagConstraints(0, // gridx
+                0, // gridy
+                1, // gridwidth
+                1, // gridheight
+                1, // weightx
+                1, // weighty
+                GridBagConstraints.NORTHEAST, // anchor <------------
+                GridBagConstraints.NONE, // fill
+                new Insets(0, // inset top
+                0, // inset left
+                0, // inset bottom
+                0), // inset right
+                0, // ipadx
+                0));
+		fpane2.add(logo,new GridBagConstraints(0, // gridx
+                0, // gridy
+                1, // gridwidth
+                1, // gridheight
+                1, // weightx
+                1, // weighty
+                GridBagConstraints.NORTH, // anchor <------------
+                GridBagConstraints.NONE, // fill
+                new Insets(10, // inset top
+                0, // inset left
+                0, // inset bottom
+                0), // inset right
+                0, // ipadx
+                0));
+		
+		
+		
+		fpane2.add(btn1,new GridBagConstraints(
+				0, // gridx
+                0, // gridy
+                1, // gridwidth
+                1, // gridheight
+                1, // weightx
+                1, // weighty
+                GridBagConstraints.NORTHWEST, // anchor <------------
+                GridBagConstraints.NONE, // fill
+                new Insets(125, // inset top
+                20, // inset left
+                0, // inset bottom
+                0), // inset right
+                0, // ipadx
+                0)
+				);
+		fpane2.add(btn2,new GridBagConstraints(
+				0, // gridx
+                0, // gridy
+                1, // gridwidth
+                1, // gridheight
+                1, // weightx
+                1, // weighty
+                GridBagConstraints.NORTHWEST, // anchor <------------
+                GridBagConstraints.NONE, // fill
+                new Insets(190, // inset top
+                20, // inset left
+                0, // inset bottom
+                0), // inset right
+                0, // ipadx
+                0)
+				);
+		fpane2.add(btn3,new GridBagConstraints(
+				0, // gridx
+                0, // gridy
+                1, // gridwidth
+                1, // gridheight
+                1, // weightx
+                1, // weighty
+                GridBagConstraints.NORTHWEST, // anchor <------------
+                GridBagConstraints.NONE, // fill
+                new Insets(255, // inset top
+                20, // inset left
+                0, // inset bottom
+                0), // inset right
+                0, // ipadx
+                0)
+				);
+		fpane2.add(btn4,new GridBagConstraints(
+				0, // gridx
+                0, // gridy
+                1, // gridwidth
+                1, // gridheight
+                1, // weightx
+                1, // weighty
+                GridBagConstraints.NORTHWEST, // anchor <------------
+                GridBagConstraints.NONE, // fill
+                new Insets(320, // inset top
+                20, // inset left
+                0, // inset bottom
+                0), // inset right
+                0, // ipadx
+                0)
+				);	
+		fpane2.add(btn5,new GridBagConstraints(
+				0, // gridx
+                0, // gridy
+                1, // gridwidth
+                1, // gridheight
+                1, // weightx
+                1, // weighty
+                GridBagConstraints.NORTHWEST, // anchor <------------
+                GridBagConstraints.NONE, // fill
+                new Insets(385, // inset top
+                20, // inset left
+                0, // inset bottom
+                0), // inset right
+                0, // ipadx
+                0)
+				);
+		//logo.setVerticalAlignment(JLabel.TOP);
+		JScrollPane jsp = new JScrollPane(fpane2);
+		frame4.add(jsp);
+		//frame4.add(date,BorderLayout.NORTH);
+		//Toolkit tk = Toolkit.getDefaultToolkit();
+		//int xSize = ((int) tk.getScreenSize().getWidth());
+		//int ySize = ((int) tk.getScreenSize().getHeight());
+		frame4.setSize(800,690);
 		frame4.setLocationRelativeTo(null);
 	}
 	
