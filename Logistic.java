f9be09fb5e49f48256c2f9990ed5148f93c81160
anirudh15015 <anirudh15015@iiitd.ac.in>
2016-11-17 16:56:38 +0530

    stores all details of logistic

--- Logistic.java
+++ Logistic.java
@@ -5,4 +5,14 @@ import java.io.*;
 public class Logistic{
 	String equipment;
 	int quantity;
+
+	Logistic(String[] a){
+		equipment=a[0];
+		quantity=Integer.parseInt(a[1]);
+	}
+
+	Logistic(){
+		
+	}
+	
 }
